a1f379643722b5487400358c0a17400d3b12cefa
Jota2kk <j.amaral4000@gmail.com>
2024-10-16 20:45:02 -0300

    adicão alerta de scroll

--- src/components/page2/page2.jsx
+++ src/components/page2/page2.jsx
@@ -1,16 +1,38 @@
 import './page2.css';
+import { useEffect, useState } from 'react';
 
-export const Page2 = ()=> {
-    return (
-            <section className="page2" id='pag2'>
-                <div className='texts'> 
-                    <h1>Ola, bem vindo</h1>
+export const Page2 = () => {
+  const [showScrollAlert, setShowScrollAlert] = useState(true);
 
-                    <p>Eu sou João Vitor</p>
-                    <a> Iniciei na área da programação a dois anos como um pequeno hobbie, aprendendo a desenvolver pequenos algoritmos para realizar pequenas tarefas no meu dia a dia... Desde então, me aprofundei na área e me encontrei como um dev full-stack web
-                    </a>
-                    <button><a href='#pag4'>Contatos</a></button>
-                </div>
-            </section>
-    )   
-}
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowScrollAlert(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <section className="page2" id="pag2">
+      <div className="texts">
+        <h1>Olá, bem-vindo</h1>
+        <p>Eu sou João Vitor</p>
+        <a>
+          Iniciei na área da programação a dois anos como um pequeno hobbie,
+          aprendendo a desenvolver pequenos algoritmos para realizar pequenas
+          tarefas no meu dia a dia... Desde então, me aprofundei na área e me
+          encontrei como um dev full-stack web.
+        </a>
+        <button><a href="#pag4">Contatos</a></button>
+
+        {showScrollAlert && (
+          <div className="scroll-alert">
+            <div className="arrow"></div>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};

6c5794a00520bde4b11d4ac334761211dd36cb1b
Jota2kk <j.amaral4000@gmail.com>
2024-11-01 02:25:09 -0300

    Adicao de imagens e atualização das descrições dos projetos

--- src/components/page3/page3.jsx
+++ src/components/page3/page3.jsx
@@ -1,7 +1,18 @@
+/* eslint-disable no-unused-vars */
 import './page3.css'
-import prjI from '../../img/prjImg.jpeg'
+import Login from '../../img/Login.png'
+import Calculadora from '../../img/Calculadora.png'
+import TempoP from '../../img/TempoP.png'
+import CRUD from '../../img/CRUD.png'
 
 export const Page3 = ()=>{
+
+    function alerta(){
+        return (
+            alert("Infelizmente o site ainda nao foi postado")
+        )
+    }
+
     return(
             <div className="page3" id='pag3'>
                 <h1 id='h1-mpj'>Meus projetos</h1>
@@ -11,17 +22,17 @@ export const Page3 = ()=>{
 
                         <div className='descri'>
 
-                            <a href='' target='_blank' className='a-imgs'><img src={prjI}/></a>
+                            <a href='' target='_blank' className='a-imgs'><img src={TempoP}/></a>
 
                             <div className='eta'>
 
-                                <h1>App Previsao Do Tempo</h1>
-                                <a>Construi um app de previsao do tempo em tempo real utilizando a whatever Api</a>
-                                <p>Node.js, ReactJs, JS, Html, Css</p>
+                                <h1>App previsao do tempo</h1>
+                                <a>Construi um app de previsao do tempo em tempo real utilizando a OpenWeather Api</a>
+                                <p>ReactJs, JavaScript, Html, Css</p>
 
                                 <div className='buttons'>
-                                    <button><a href=''  target='_blank'> Deploy </a></button>
-                                    <button><a href='https://github.com/Jota2kk/tela-novo-usuario'  target='_blank'>GitHub</a></button>
+                                    <button><a href='' target='_blank'>Deploy</a></button>
+                                    <button><a href='https://github.com/Jota2kk/app-tempoP'  target='_blank'>GitHub</a></button>
                                 </div>
 
                             </div>
@@ -34,16 +45,16 @@ export const Page3 = ()=>{
 
                        <div className='descri'>
 
-                            <a href='' target='_blank' className='a-imgs'><img src={prjI}/></a>
+                            <a href='' onClick={alerta} className='a-imgs'><img src={CRUD}/></a>
 
                             <div className='eta'>
 
-                                <h1 className='prjs-name'>Tela de login</h1>
-                                <a>Site feito com React, Node.js e o banco de dados Mysql. Envia todas as informações de um novo usuario para o banco de dados e a mesmo tempo expoe em tela os usarios ja cadastrados.</a>    
-                                <p>Node.js, ReactJs, JS, Html, Css e MySql</p>
+                                <h1 className='prjs-name'>CRUD</h1>
+                                <a>CRUD que expõe os dados do usuario em tela</a>    
+                                <p>Node.js, ReactJs, JavaScript, Html, Css e MySql</p>
 
                                 <div className='buttons'>
-                                    <button><a href=''  target='_blank'> Deploy </a></button>
+                                    <button><a href='' onClick={alerta}> Deploy </a></button>
                                     <button><a href='https://github.com/Jota2kk/tela-novo-usuario'  target='_blank'>GitHub</a></button>
                                 </div>
 
@@ -56,13 +67,13 @@ export const Page3 = ()=>{
                     <div className='prj3'>            
                         <div className='descri'>
 
-                            <a href='' target='_blank' className='a-imgs'><img src={prjI}/></a>
+                            <a href='' onClick={alerta} className='a-imgs'><img src={Login}/></a>
     
                             <div className='eta'>
 
                                 <h1 className='prjs-name'>Tela de login</h1>
-                                <a>Site feito com React, Node.js e o banco de dados Mysql. Envia todas as informações de um novo usuario para o banco de dados e a mesmo tempo expoe em tela os usarios ja cadastrados.</a>    
-                                <p>Node.js, ReactJs, JS, Html, Css e MySql</p>
+                                <a>Tela de login simples, projeto feito com TailWind CSS </a>    
+                                <p>Reactjs,TailWind CSS</p>
 
                                 <div className='buttons'>
                                     <button><a href=''  target='_blank'> Deploy </a></button>
@@ -79,17 +90,17 @@ export const Page3 = ()=>{
 
                         <div className='descri'>
 
-                            <a href='' target='_blank' className='a-imgs'><img src={prjI}/></a>
+                            <a href='https://jota2kk.github.io/Calculadora/' target='_blank' className='a-imgs'><img src={Calculadora}/></a>
 
                             <div className='eta'>
 
-                                <h1 className='prjs-name'>Tela de login</h1>
-                                <a>Site pradonizado para exposição de cardapio de uma hamburgueria. feito com React</a>
-                                <p>Node.js, ReactJs, JS, Html, Css e MySql</p>
+                                <h1 className='prjs-name'>Calculadora</h1>
+                                <a>Calculadora responsiva</a>
+                                <p>Html, Css e JavaScript</p>
 
                                 <div className='buttons'>
-                                    <button><a href=''  target='_blank'> Deploy </a></button>
-                                    <button><a href='https://github.com/Jota2kk/tela-novo-usuario'  target='_blank'>GitHub</a></button>
+                                    <button><a href='https://jota2kk.github.io/Calculadora/'  target='_blank'> Deploy </a></button>
+                                    <button><a href='https://github.com/Jota2kk/Calculadora'  target='_blank'>GitHub</a></button>
                                 </div>
 
                             </div>
